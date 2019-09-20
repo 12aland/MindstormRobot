e5e5c243ff43630b8a8e5f42065fdac894511d42
12aland <buzz2lite@gmail.com>
2019-09-20 14:39:52 -0700

    v1.0.1.1

--- src/main/java/MainProgram/Commands/HelloRobot.java
+++ src/main/java/MainProgram/Commands/HelloRobot.java
@@ -1,6 +1,8 @@
 package MainProgram.Commands;
 
 import Robots.Robot;
+import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 
 public class HelloRobot {
     private Robot rbt;
@@ -11,5 +13,6 @@ public class HelloRobot {
 
     public void mainScript(){
         System.out.println(rbt.whoami());
+        EV3LargeRegulatedMotor mA = new EV3LargeRegulatedMotor(MotorPort.A);
     }
 }
